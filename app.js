102d874d70fe0022792e1a7a75f7b5548629e557
karinagalindo <34430911+karinagalindo@users.noreply.github.com>
2018-07-12 22:41:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var mongoose=require("mongoose");
 
 var path=require("path");
 var bodyParser=require("body-parser");
-var cookiesParser=require("cookie-parser");
+var cookieParser=require("cookie-parser");
 var session =require("express-session");
 var flash=require("connect-flash");
 
 var passport=require("passport");
 
 var routes=require("./routes");
-//var passportsetup=require("/.passportsetup");
+var passportsetup=require("./passportsetup");
 var app=express();
 
 mongoose.connect("mongodb://localhost:27017/zombie_nest");
 
-//passportsetup();
+passportsetup();
 
 app.set("port",process.env.PORT || 3000);
 
@@ -23,7 +23,7 @@ app.set("views",path.resolve(__dirname,"views"));
 app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(cookiesParser());
+app.use(cookieParser());
 app.use(session({
 secret:"UELlksdjelifnrJIUH389#$#!DJskkokjPJJioi",
 resave:true,
@@ -31,6 +31,10 @@ saveUninitialized:true
 }));
 app.use(flash());
 
+app.use(passport.initialize({
+    userProperty:"zombie"
+}));
+app.use(passport.session());
 
 app.use(routes);
 
--- routes.js
+++ routes.js
@@ -2,15 +2,29 @@ var express=require("express");
 var Zombie=require("./models/zombie");
 var Arma=require("./models/arma")
 var passport=require("passport");
-
+var acl =  require('express-acl');
 var router=express.Router();
+
+acl.config({
+    baseUrl:'/',
+    defaultRole:'invitado',
+    decodedObjectName:'zombie',
+    roleSearchPath:'zombie.role'
+});
+router.use(acl.authorize);
 
 router.use((req,res,next)=>{
     res.locals.currentZombie=req.zombie;
     res.locals.errors=req.flash("error");
     res.locals.infos=req.flash("info");
+    if(req.isAuthenticated()){
+        req.session.role=req.zombie.role;
+    }
+    console.log(req.session);
     next();
 });
+router.use(acl.authorize);
+
 router.use((req,res,next)=>{
     res.locals.currentZArma=req.arma;
     res.locals.errors=req.flash("error");
@@ -47,10 +61,38 @@ router.get("/singup",(req,res)=>{
 router.get("/armas",(req,res)=>{
 res.render("armas");
 });
+router.get("/login",(req,res)=>{
+    res.render("login");
+});
+router.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+router.get("/edit",ensureAuthenticated,(req,res)=>{
+    res.render("/edit");
+});
+router.post("/edit",ensureAuthenticated,(req,res,next)=>{
+    req.zombie.displayName=req.body.displayName;
+    req.zombie.bio=req.body.bio;
+    req.zombie.save((err)=>{
+        if(err){
+            return;
+        }
+        req.flash("info","Perfil actualizado");
+        res.redirect("/edit");
+    });
+});
+
+router.post("/login",passport.authenticate("login",{
+    successRedirect:"/",
+    failureRedirect:"/login",
+    failureFlash:true
+}));
 
 router.post("/singup",(req,res,next)=>{
     var username=req.body.username;
     var password= req.body.password;
+    var role =req.body.role;
 
 Zombie.findOne({username:username},(err,zombie)=>{
 if(err){
@@ -62,27 +104,27 @@ if(zombie){
 }
 var newZombie = new Zombie({
     username:username,
-    password:password
+    password:password,
+    role:role
 });
 newZombie.save(next);
 return res.redirect("/");
 });
 });
+function ensureAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+next();
+    }else{
+        req.flash("info","Necesita iniciar sesion para poder ver esta seccion");
+        res.redirect("/login");
+    }
+}
 
-
-router.post("/arma",(req,res,next)=>{
+router.post("/armas",(req,res,next)=>{
     var descripcion=req.body.descripcion;
     var categoria= req.body.categoria;
     var fuerza= req.body.fuerza;
 
-/*Arma.findOne({descripcion:descripcion},(err,arma)=>{
-if(err){
-    return next(err);
-}
-if(arma){
-    req.flash("error","El nombre de usuario ya lo ha formado otra arma");
-    return res.redirect("/armas");
-}*/
 var newArma = new Arma({
     descripcion:descripcion,
     categoria:categoria,
